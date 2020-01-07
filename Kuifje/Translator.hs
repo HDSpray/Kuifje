b4c1bb16d18b213ba7b0036b971880605cbec6bc
HDSpary <jdbde@outlook.com>
2020-01-07 19:40:47 +1100

    some feature of the translator

--- Kuifje/Translator.hs
+++ Kuifje/Translator.hs
@@ -51,6 +51,10 @@ aOperator op d1 d2 =
 cOperator op d1 d2 = 
   D $ fromListWith (+) [((Left (op x y)), p * q) | (Right x, p) <- toList $ runD d1,
                                                     (Right y, q) <- toList $ runD d2]
+bOperator op d1 d2 = 
+  D $ fromListWith (+) [((Left (op x y)), p * q) | (Left x, p) <- toList $ runD d1,
+                                                    (Left y, q) <- toList $ runD d2]
+
 
 
 evalEE :: Expr -> (Gamma ~> (Either Bool Rational))
@@ -58,19 +62,45 @@ evalEE (Var id) = \s -> case E.lookup s id of
                           Just (R r) -> (return (Right r))
                           Just (B b) -> (return (Left b))
 evalEE (RationalConst r) = \s -> return (Right r)
--- evalEE (Neg r) = \s -> let f = evalEE r in (fmap ((*) -1) (f s))
+evalEE (Neg r) = \s -> let r' = (evalEE r) s in 
+                           (fmap (\p -> case p of 
+                                          (Right p') -> Right (-1 * p'))) r'
 evalEE (ABinary op e1 e2) = \s -> 
   let e1' = (evalEE e1) s
       e2' = (evalEE e2) s in 
       case op of 
-        Add ->      (aOperator (+) e1' e2')
+        Add      -> (aOperator (+) e1' e2')
         Subtract -> (aOperator (-) e1' e2')
+        Multiply -> (aOperator (*) e1' e2')
+        Divide   -> (aOperator (/) e1' e2')
+{-
+evalEE (Ichoice e1 e2 p) = \s -> 
+  let e1' = (evalEE e1) s
+      e2' = (evalEE e2) s 
+      p'  = (evalEE p) s in 
+      case (e1', e2') of 
+        (D (Right e1''), e2') -> undefined
+    - -}
 evalEE (BoolConst b) = \s -> return (Left b)
+evalEE (Not b) = \s -> let r' = (evalEE b) s in 
+                           (fmap (\bv -> case bv of 
+                                          (Left b') -> Left (not b'))) r'
+evalEE (BBinary op e1 e2) = \s -> 
+  let e1' = (evalEE e1) s
+      e2' = (evalEE e2) s in 
+      case op of 
+        And ->      (bOperator (&&) e1' e2')
+        Or  ->      (bOperator (||) e1' e2')
 evalEE (RBinary op e1 e2) = \s -> 
   let e1' = (evalEE e1) s
       e2' = (evalEE e2) s in 
       case op of 
         Gt ->      (cOperator (>) e1' e2')
+        Ge ->      (cOperator (>=) e1' e2')
+        Lt ->      (cOperator (<) e1' e2')
+        Le ->      (cOperator (<=) e1' e2')
+        Eq ->      (cOperator (==) e1' e2')
+        Ne ->      (cOperator (/=) e1' e2')
 
                                  {-
 evalE :: Expr -> Either (Gamma ~> Bool) (Gamma ~> Rational)
@@ -178,6 +208,16 @@ translateKuifje (Syntax.While e s) =
         Language.Kuifje.Syntax.while (\s -> 
                 let currS = (evalEE e) s in 
                     fmap (\r -> case r of (Left b) -> b) currS) (translateKuifje s)
+translateKuifje (Syntax.If e s1 s2) = 
+    Language.Kuifje.Syntax.cond 
+        (\s -> let currS = (evalEE e) s in fmap (\r -> case r of (Left b) -> b) currS) 
+        (translateKuifje s1) 
+        (translateKuifje s2)
+translateKuifje Syntax.Skip = skip
+translateKuifje (Leak e) = undefined
+translateKuifje (Vis s) = undefined
+translateKuifje (Echoice s1 s2 p) = undefined
+
 
 
 getRational :: Gamma -> String -> Rational
@@ -196,9 +236,9 @@ hyper = let g = translateKuifje (Parse.parseString example) in
             project $ hysem g (uniform [initGamma x | x <- [5..8]])
 
 example :: String
--- example = "x := 1; y := 0; z := z + 1; while (x > 0) do y := x + y; x := x - 1; od;"
+example = "y := 0; while (x > 0) do y := x + y; x := x - 1; od;"
 
-example = "y := x + 1;"
+-- example = "y := x + 1;"
           
 main :: IO ()
 main = do
